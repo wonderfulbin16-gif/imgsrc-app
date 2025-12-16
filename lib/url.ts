8faf6c6154894cfe101953f9a37a7bc395c313db
wonderfulbin16-gif <wonderful.bin16@gmail.com>
2025-12-16 11:34:39 +0800

    Update url.ts

--- lib/url.ts
+++ lib/url.ts
@@ -1,5 +1,6 @@
 export function absoluteUrl(path: string) {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+  const baseUrl =
+    process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000'
   const absoluteUrl = new URL(path, baseUrl)
   return absoluteUrl.toString()
 }
